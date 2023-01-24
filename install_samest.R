a03855b121d3c43c94661e66abbe3d4f9546b708
Dan Greenberg <dan.greenberg01@gmail.com>
2023-01-24 08:41:37 -0800

    reduced iter for stan test

--- install_samest.R
+++ install_samest.R
@@ -42,8 +42,8 @@ fit1 <- stan(
   file = "code/stan/ricker_linear.stan",  # Stan program
   data = standata,    # named list of data
   chains = 4,             # number of Markov chains
-  warmup = 1000,          # number of warmup iterations per chain
-  iter = 2000,            # total number of iterations per chain
+  warmup = 100,          # number of warmup iterations per chain
+  iter = 200,            # total number of iterations per chain
   cores = 1,              # number of cores (could use one per chain)
   refresh = 0             # no progress shown
   )
